5d8ef797ee10fe45dafde5935eda85c9fa2c836f
chief-masik <bykallikaheu@gmail.com>
2023-04-15 03:17:06 +0300

    mod

--- app/src/main/java/com/example/firstApplication/MainActivity.java
+++ app/src/main/java/com/example/firstApplication/MainActivity.java
@@ -1,19 +1,12 @@
 package com.example.firstApplication;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
